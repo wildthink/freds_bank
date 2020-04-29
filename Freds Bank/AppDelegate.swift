2a4fc7c25158903ed937dd04552b5ffc3710ddd1
Jason Jobe <box2019@jasonjobe.com>
2020-04-28 23:29:54 -0400

    added  tests

--- Freds Bank/AppDelegate.swift
+++ Freds Bank/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         appViewModel.delegate = self
-        modelWillCommit(appViewModel)
+        modelWillCommit(baseViewModel)
         return true
     }
 }
--- Freds BankTests/Freds_BankTests.swift
+++ Freds BankTests/Freds_BankTests.swift
@@ -7,28 +7,97 @@
 //
 
 import XCTest
+import SnapshotTesting
+
 @testable import Freds_Bank
+@testable import SQiftViewModel
 
 class Freds_BankTests: XCTestCase {
 
+    var viewModel: AppViewModel!
+    var storyboard: UIStoryboard!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+//        record = true
+        viewModel = try! AppViewModel(storageLocation: .onDisk("/Users/jason/bank_test.db"))
+        let mb = Bundle(for: AppDelegate.self)
+        storyboard = UIStoryboard(name: "Main", bundle: mb)
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAppDelegate() {
+        let appDelegate = AppDelegate()
+        appDelegate.modelWillCommit(appDelegate.baseViewModel)
+    }
+    
+    func testViewModel() throws {
+        try viewModel.clear(env: "demo")
+        try viewModel.clear(log: "demo")
+        viewModel.set(env: "search", to: "keyw")
+        _ = viewModel.sql_predicate(field: "name", search: "search")
+        
+        let path = Bundle(for: AppDelegate.self).path(forResource: "branches", ofType: "json")
+        try viewModel.load(path, into: "_branches")
+        let url = URL(fileURLWithPath: path!)
+        viewModel.load(url: url, into: "_branches")
+    }
+    
+    func testMapController() throws {
+        let map = storyboard.instantiateViewController(identifier: "MapController") as! MapViewController
+        checkViewController(map) {
+            if let pin = $0.items.first {
+                $0.mapView?.selectAnnotation(pin, animated: false)
+            }
+            $0.selectMapItem(self)
+            
+            assertSnapshot(matching: $0, as: .dump)
+            assertSnapshot(matching: $0, as: .image(on: .iPhoneSe))
+
+            $0.mapView($0.mapView!, regionDidChangeAnimated: false)
+            $0.saveSelectedLocation()
+            let seg = UIStoryboardSegue(identifier: "jump", source: $0, destination: $0)
+            $0.prepare(for: seg, sender: nil)
+        }
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testTableController() throws {
+        let vc = storyboard.instantiateViewController(identifier: "TableController") as! TableViewController
+        checkViewController(vc) {
+            $0.tableView($0.tableView, didSelectRowAt: IndexPath(row: 0, section: 0))
+            assertSnapshot(matching: $0, as: .image(on: .iPhoneSe))
+            assertSnapshot(matching: $0, as: .recursiveDescription(on: .iPhoneSe))
+            let seg = UIStoryboardSegue(identifier: "jump", source: $0, destination: $0)
+            $0.prepare(for: seg, sender: nil)
         }
+    }
+
+    func testDetailController() throws {
+        let vc = storyboard.instantiateViewController(identifier: "DetailController") as! DetailViewController
+        vc.modelId = "locations/selected.branch"
+        viewModel.set(env: "selected.branch", to: 1)
+            checkViewController(vc) {
+            assertSnapshot(matching: $0, as: .image(on: .iPhoneSe))
+            assertSnapshot(matching: $0, as: .recursiveDescription(on: .iPhoneSe))
+        }
+    }
+
+    func testSignaling() throws {
+        viewModel.watchingForSignals = true
+        viewModel.set(env: "signal", to: "Sender is \(#function)")
+        sleep(1)
+        viewModel.watchingForSignals = false
+    }
+    
+    func checkViewController<VC: UIViewController>(_ vc: VC, f: ((VC) -> Void)? = nil) {
+        _ = vc.view // force loading of the view
+        vc.viewWillAppear(false)
+        vc.refresh(from: viewModel)
+        f?(vc)
+        vc.viewWillDisappear(false)
+        
     }
 
 }
